c989fc97b2f9e61916a3f46e9f5a8783edafce02
santiagodev <sanxp18@gmail.com>
2024-08-26 21:39:13 -0400

    finish logic to change the sharing section for mobile size

--- script.js
+++ script.js
@@ -7,51 +7,45 @@ const paragraphContainer = document.querySelector(".sharing-section__paragraph-c
 // const socialMediaSection = document.querySelector(".social-media-section");
 console.log(shareButton);
 
-
 shareButton.addEventListener("click", changeSharingSection);
 
 function changeSharingSection() {
     //tengo que poner una condicion que a partir de un width de 660px, no se le quite la clase de main-content__sharing-section
-    const profileImageHidden = profileImage.style.display = "none";
-    const paragraphContainerHidden = paragraphContainer.style.display = "none";
-    console.log(profileImageHidden);
+    profileImage.classList.toggle("inactive");
+    paragraphContainer.classList.toggle("inactive");
+    const profileImageHidden = profileImage.classList.contains("inactive");
+    const paragraphContainerHidden = paragraphContainer.classList.contains("inactive");
+    const shareParagraph = sharingSection.appendChild(document.createElement("p"));
+    const socialMediaIconsContainer = sharingSection.appendChild(document.createElement("div"));
     
-
-    if(profileImageHidden && paragraphContainerHidden ) {
+    if(profileImageHidden && paragraphContainerHidden) {
         //muestra la seccion de redes sociales
-        sharingSection.classList.remove(".main-content__sharing-section");
+        sharingSection.classList.remove("main-content__sharing-section");
         sharingSection.classList.add("social-media-section");
 
         //creando elementos en la seccion de redes sociales
-        const newPSharingSection = sharingSection.appendChild(document.createElement("p"));
-        newPSharingSection.textContent = "SHARE"
-        newPSharingSection.classList.add("social-media-section__paragraph");
-
-        const iconsContainer = sharingSection.appendChild(document.createElement("div"));
-        iconsContainer.classList.add("social-media__icons-container");
-        iconsContainer.innerHTML = `<a href="/"><img src="./article-preview-component-master/images/icon-facebook.svg" alt="facebook-image"></a>
+        console.log(shareParagraph);
         
-        <a href="/"><img src="./article-preview-component-master/images/icon-twitter.svg" alt="twitter-image"></a>
-
-        <a href="/"><img src="./article-preview-component-master/images/icon-pinterest.svg" alt="pinterest-image"></a>
-        `
+        shareParagraph.textContent = "SHARE"
+        shareParagraph.classList.add("social-media-section__paragraph");
+    
+        socialMediaIconsContainer.classList.add("social-media__icons-container");
+        socialMediaIconsContainer.innerHTML = `<a href="/"><img src="./article-preview-component-master/images/icon-facebook.svg" alt="facebook-image"></a>
+            
+            <a href="/"><img src="./article-preview-component-master/images/icon-twitter.svg" alt="twitter-image"></a>
+    
+            <a href="/"><img src="./article-preview-component-master/images/icon-pinterest.svg" alt="pinterest-image"></a>
+            `
+     } else if (!profileImageHidden && !paragraphContainerHidden){
+        //aqui hay que borrar la social media section
+        const shareParagraphStyles = document.querySelector(".social-media-section__paragraph");
+        const socialMediaIconsStyles = document.querySelector(".social-media__icons-container");
+        shareParagraphStyles.remove();
+        socialMediaIconsStyles.remove();
+        shareParagraph.remove();
+        socialMediaIconsContainer.remove();
         
-
-
-
-        // const iconFacebookSharingSection = sharingSection.appendChild(document.createElement("a"));
-        // const iconTwitterSharingSection = sharingSection.appendChild(document.createElement("a"));
-        // const iconPinterestSharingSection = sharingSection.appendChild(document.createElement("a"));
-        // const facebookImg = new Image(10, 30);
-        // const twitterImg = new Image(10, 30);
-        // const pinterestImg = new Image(10, 30);
-        // facebookImg.src = "./article-preview-component-master/images/icon-facebook.svg";
-        // twitterImg.src = "./article-preview-component-master/images/icon-twitter.svg";
-        // pinterestImg.src = "./article-preview-component-master/images/icon-pinterest.svg";
-        // const imageInsideFacebookIcon = iconFacebookSharingSection.appendChild(facebookImg);
-        // const imageInsideTwitterIcon = iconTwitterSharingSection.appendChild(twitterImg);
-        // const imageInsidePinterestIcon = iconPinterestSharingSection.appendChild(pinterestImg);
-
-
+        sharingSection.classList.remove("social-media-section");
+        sharingSection.classList.add("main-content__sharing-section");
     }
 }
